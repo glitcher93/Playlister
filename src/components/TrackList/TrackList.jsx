3c097ca9b2b6bc5164bc798862081f47a7e2b967
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 15:09:00 -0500

    created TrackList component and its layout

---
+++ src/components/TrackList/TrackList.jsx
@@ -0,0 +1,11 @@
+import './TrackList.scss';
+
+function TrackList() {
+    return (
+        <div class="trackList">
+            {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        </div>
+    )
+}
+
+export default TrackList;
