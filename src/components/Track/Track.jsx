52fede57e80c741104649258e29ffa27ef59d7c2
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 15:54:23 -0500

    rendered track.name, track.artist, and track.album in component

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,11 +1,11 @@
 import './Track.scss';
 
-function Track() {
+function Track({ track }) {
     return (
         <article className="track">
             <div className="track__information">
-                <h3 className='track__name'></h3> {/* <!-- track name will go here --> */}
-                <p className='track__artist'></p> {/* <!-- track artist will go here--> | <!-- track album will go here --> */}
+                <h3 className='track__name'>{track.name}</h3> {/* <!-- track name will go here --> */}
+                <p className='track__artist'>{`${track.artist} | ${track.album}`}</p> {/* <!-- track artist will go here--> | <!-- track album will go here --> */}
             </div>
             <button class="track__button"></button> {/* <!-- + or - will go here --> */}
         </article>
