9dfe6af25a2a479df3efcacb8a4a73fac6c7c1e9
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 12:02:26 -0500

    created SearchResults component and its basic layout

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,12 @@
+import './SearchResults.scss'
+
+function SearchResults() {
+    return (
+        <div className="search-results">
+            <h2 className='search-results__title'>Results</h2>
+            {/* TrackList */}
+        </div>
+    )
+}
+
+export default SearchResults;
