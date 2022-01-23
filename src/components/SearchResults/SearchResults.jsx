690c4d26ab83389763f171a0927eada3adb0c41a
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 18:12:42 -0500

    passed onAdd and isRemoval down to Tracklist

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,11 +1,15 @@
 import './SearchResults.scss';
 import TrackList from '../TrackList';
 
-function SearchResults({ searchResults }) {
+function SearchResults({ searchResults, onAdd }) {
     return (
         <div className="search-results">
             <h2 className='search-results__title'>Results</h2>
-            <TrackList tracks={searchResults} />
+            <TrackList 
+            tracks={searchResults}
+            onAdd={onAdd}
+            isRemoval={false} 
+            />
         </div>
     )
 }
