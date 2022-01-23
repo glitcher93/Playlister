761d5fed45ed33f3928f979501bc07dc2ef861d1
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 18:14:49 -0500

    created addTrack, and renderButton function, add an onClick with a value of addTrack

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,13 +1,30 @@
 import './Track.scss';
 
-function Track({ track }) {
+function Track({ track, onAdd, isRemoval }) {
+
+    const addTrack = () => {
+        onAdd(track);
+    }
+
+    const renderButton = () => {
+        if (isRemoval) {
+            return <button class="track__button">-</button>
+        }
+        return <button 
+                class="track__button"
+                onClick={addTrack}
+                >
+                    +
+                </button>
+    }
+
     return (
         <article className="track">
             <div className="track__information">
                 <h3 className='track__name'>{track.name}</h3> {/* <!-- track name will go here --> */}
                 <p className='track__artist'>{`${track.artist} | ${track.album}`}</p> {/* <!-- track artist will go here--> | <!-- track album will go here --> */}
             </div>
-            <button class="track__button"></button> {/* <!-- + or - will go here --> */}
+            {renderButton()}
         </article>
     )
 }
