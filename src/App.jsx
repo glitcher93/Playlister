22cc05dac5476c5191cfac69a6ecb1d5f4bcab04
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 15:51:27 -0500

    created state for searchResults, and passed it down to SearchResults

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.scss';
 import Playlist from './components/Playlist';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
 
 function App() {
+
+  const [searchResults, setSearchResults] = useState([{
+    name: 'Track 1',
+    artist: 'Lol',
+    album: 'Lol',
+    id: 1
+  }])
+
   return (
     <>
       <header className='header'>
@@ -12,7 +21,7 @@ function App() {
       <main className='main'>
         <SearchBar />
         <div className="main__container">
-          <SearchResults />
+          <SearchResults searchResults={searchResults} />
           <Playlist />
         </div>
       </main>
