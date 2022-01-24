944d99ce43ae1c84e49837d8c7dfcf5941fb1241
glitcher93 <nigeljdsouza@gmail.com>
2022-01-23 19:25:16 -0500

    created state for term, created search and handleTermChange function, passed search to onClick on button element, passed handleTermChange to onChange on input element

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import './SearchBar.scss';
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+
+    const [term, setSearchTerm] = useState('')
+
+    const search = () => {
+        onSearch(term)
+    }
+
+    const handleTermChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
     return (
         <div className='search-bar'>
             <input 
+            value={term}
             type="text" 
             name="search" 
             id="search"
             className='search-bar__input' 
-            placeholder='Enter A Song, Album, or Artist' 
+            placeholder='Enter A Song, Album, or Artist'
+            onChange={handleTermChange} 
             />
-            <button className='search-bar__button'>
+            <button 
+            className='search-bar__button'
+            onClick={search}
+            >
                 Search
             </button>
         </div>
